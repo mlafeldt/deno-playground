8f8364f91387b3dc08d65ec40bab468bc4c05a8c
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2023-04-19 14:16:11 +0200

    Terminate isolate after timeout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::rc::Rc;
+use tokio::time;
 
 use deno_core::{anyhow::Result, resolve_path, v8, FsModuleLoader, JsRuntime, RuntimeOptions};
 
@@ -8,9 +9,10 @@ async fn main() -> Result<()> {
     let cwd = env::current_dir()?;
     let module_path = env::args().nth(1).unwrap_or_else(|| "hello.js".to_string());
     let module_spec = resolve_path(&module_path, &cwd)?;
-    dbg!(module_path);
+    println!("Running {module_path}");
 
-    let memory_limit = 100 * 1024 * 1024;
+    let memory_limit = 10 * 1024 * 1024;
+    let timeout = time::Duration::from_secs(2);
 
     let mut runtime = JsRuntime::new(RuntimeOptions {
         module_loader: Some(Rc::new(FsModuleLoader)),
@@ -21,9 +23,20 @@ async fn main() -> Result<()> {
     // Terminate isolate when approaching memory limit
     let cb_handle = runtime.v8_isolate().thread_safe_handle();
     runtime.add_near_heap_limit_callback(move |current_limit, _initial_limit| {
-        dbg!(current_limit);
+        println!("Terminating isolate near memory limit (current={current_limit} max={memory_limit})");
         cb_handle.terminate_execution();
         current_limit * 2
+    });
+
+    // Start controller thread to terminate isolate after timeout
+    let cb_handle = runtime.v8_isolate().thread_safe_handle();
+    tokio::spawn(async move {
+        tokio::select! {
+            _ = tokio::time::sleep(timeout) => {
+                println!("Terminating isolate after timeout ({timeout:?})");
+                cb_handle.terminate_execution();
+            }
+        }
     });
 
     let module_id = runtime.load_main_module(&module_spec, None).await?;
@@ -33,7 +46,7 @@ async fn main() -> Result<()> {
       biased;
 
       maybe_result = &mut receiver => {
-        dbg!(&maybe_result);
+        println!("Result = {maybe_result:#?}");
         maybe_result.expect("Module evaluation result not provided.")
       }
 
